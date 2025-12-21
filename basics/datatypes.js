8e992c3ca22b1a4cc871f58c3f110685a4ab8cad
yogeshwarbhardwaj <157011412+yogeshwarbhardwaj@users.noreply.github.com>
2025-12-21 11:32:20 +0000

    container config done

---
+++ basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+//alert(3+3) // we are using node js not browser
+console.log(3+3) // code readibility shoud be high
+console.log("yog")
+let name = "yogesh"
+let age = 18
+let isloggedin = false
+let state;
+// number => 2 to power 53
+// bigint
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+ // symbol => unique
+
+// object
+console.log(typeof age)
+console.log(typeof null) // object
+console.log(typeof undefined)
