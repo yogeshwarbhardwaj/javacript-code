2c5a11dde9e0c13445a62b746ddc32501017232d
yogeshwarbhardwaj <157011412+yogeshwarbhardwaj@users.noreply.github.com>
2025-12-21 11:04:27 +0000

    container config done

---
+++ basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accountEmail = "yogesh@gmail.com"
+var accountpassword = "jaipur"
+// account id = 2 // not allowed
+console.log(accountId);
+accountEmail = "hc@hc.com"
+accountpassword = "212121121"
+accountcity = "bengaluru"
+console.table([accountpassword,accountEmail,accountId,accountcity]);
