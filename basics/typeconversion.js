d0ac2109adb5859a30423e3128480ccb666f8202
yogeshwarbhardwaj <157011412+yogeshwarbhardwaj@users.noreply.github.com>
2025-12-24 03:34:22 +0000

    conversion of data type

---
+++ basics/typeconversion.js
@@ -0,0 +1,21 @@
+let score = "33abc";
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+// "33" => 33
+// "33abc" => NAN
+// true => 1;false => 0
+let isloggedin = "hitesh"
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin);
+// 1 => true; 0 => false
+// " " => false
+// "hitesh" = true
+let somenumber = 33
+let StringNumber = String(somenumber)
+console.log(StringNumber)
+console.log(typeof StringNumber)
+
+
