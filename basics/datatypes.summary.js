74a087245428a68747b44b61dd1c298f679b1a97
yogeshwarbhardwaj <157011412+yogeshwarbhardwaj@users.noreply.github.com>
2025-12-24 04:50:12 +0000

    datatypes summary done

---
+++ basics/datatypes.summary.js
@@ -0,0 +1,30 @@
+//primitie datatype
+// 7 types : string,number , boolean , null 
+const score = 100
+const scoreValue = 100.3
+const isloggedin = false
+const outsidetemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(anotherid == id);
+const bigNumber = 345356543n
+// reference (non primitive)
+// array, objects,functions
+// declaration of array
+const heros = ["maharanpratap", "shivaji" , " gurugovindsingh"];
+// objects
+let myobj = {
+      name: "yogesh",
+      age:  22,
+
+}
+// functions
+const myfunction = function(){
+    console.log("hello yogi")
+}
+console.log(typeof bigNumber)
+console.log(typeof outsidetemp);
+
+
+
