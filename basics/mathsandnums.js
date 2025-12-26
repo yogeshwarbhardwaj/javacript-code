b01eb07bbbf47f6a622a915a7ae6407f1a5e2db0
yogeshwarbhardwaj <157011412+yogeshwarbhardwaj@users.noreply.github.com>
2025-12-26 15:05:22 +0000

    math and nums

---
+++ basics/mathsandnums.js
@@ -0,0 +1,46 @@
+const score = 400
+//console.log(score)
+const balance =new Number(100)
+//console.log(balance)
+// console.log(balance.toString());
+//console.log(balance.toFixed(2));
+//const othernumber = 1123.8966
+//console.log(othernumber.toPrecision(4));
+//console.log(othernumber.toPrecision(3));
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+//////////////////maths/////////
+// console.log(Math)
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.92))
+// console.log((Math.ceil(4.6)));
+// console.log((Math.floor(4.9)));
+// console.log((Math.min(3,4,5,6)));
+// console.log(Math.max(4,5,6,3));
+console.log(Math.random());
+console.log((Math.random() *10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+const min = 10;
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
