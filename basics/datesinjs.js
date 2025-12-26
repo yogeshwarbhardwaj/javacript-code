063dd8991b838790cf340a84cb689902d0eb57d7
yogeshwarbhardwaj <157011412+yogeshwarbhardwaj@users.noreply.github.com>
2025-12-26 15:53:26 +0000

    dates completes

---
+++ basics/datesinjs.js
@@ -0,0 +1,36 @@
+// dates 
+let mydate = new Date()
+console.log(mydate.toString());
+console.log(mydate.toDateString);
+console.log(mydate.toLocaleString);
+console.log(typeof mydate);
+
+
+
+
+
+
+//let mycreateddate = new Date(2025,0,3)
+//console.log(mycreateddate);
+//let mycreateddate = new Date(2023,0,23,5,3)
+//console.log(mycreateddate.toLocaleString());
+let mycreateddate = new Date("01-14-2023")
+//console.log(mycreateddate);
+let myTimeStamp = Date.now()
+//console.log(myTimeStamp);
+//console.log(mycreateddate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
