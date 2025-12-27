ccf5bf687b4f113ad3c7728d31ca455f967fa820
yogeshwarbhardwaj <157011412+yogeshwarbhardwaj@users.noreply.github.com>
2025-12-27 06:10:43 +0000

    objectbasics doe

---
+++ 02_basics/object03.js
@@ -0,0 +1,34 @@
+// singleton
+//object.create 
+// object literals
+const mysym = Symbol("key1")
+const jsuser = {
+    name: "yogeshwar",
+    "full name" : "yogeshwar bhadwaj",
+    [mysym] : "mykey1",
+    age:20,
+    location: "jaipur",
+    email: "bhardwajyogeshwar@google.com",
+    isloggedin:false,
+    lastlogindays:["Monday","saturday"]
+
+}
+// console.log(jsuser.email);
+// console.log(jsuser["email"]);
+// console.log(jsuser["full name"]);
+// console.log(jsuser[mysym]);
+jsuser.email = "yogesh@chatgpt.com"
+Object(jsuser)
+jsuser.email = "yogesh@linkedin.com"
+console.log(jsuser);
+jsuser.greeting = function() {
+    console.log("hello js user");
+    
+    
+}
+console.log((jsuser.greeting()));
+
+
+
+
+
