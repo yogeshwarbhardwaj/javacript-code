75a2b22565f12cf93d1ea3b5e243d2601fdab535
yogeshwarbhardwaj <157011412+yogeshwarbhardwaj@users.noreply.github.com>
2025-12-27 03:22:47 +0000

    date completed

--- basics/datesinjs.js
+++ basics/datesinjs.js
@@ -1,9 +1,9 @@
 // dates 
 let mydate = new Date()
-console.log(mydate.toString());
-console.log(mydate.toDateString);
-console.log(mydate.toLocaleString);
-console.log(typeof mydate);
+//console.log(mydate.toString());
+//console.log(mydate.toDateString);
+//console.log(mydate.toLocaleString);
+//console.log(typeof mydate);
 
 
 
@@ -20,6 +20,17 @@ let myTimeStamp = Date.now()
 //console.log(myTimeStamp);
 //console.log(mycreateddate.getTime());
 console.log(Math.floor(Date.now()/1000));
+let newDate =  new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+// `${newDate.getDay()} and the time `
+newDate.toLocaleString('deafault',{
+    weekday: "long"
+     
+})   
+
+
 
 
 
