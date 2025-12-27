8c1561229672cf6991790a6813c0ccc32f862bc4
yogeshwarbhardwaj <157011412+yogeshwarbhardwaj@users.noreply.github.com>
2025-12-27 04:09:39 +0000

    array first part done

---
+++ 02_basics/array.js
@@ -0,0 +1,42 @@
+const myArr = [0,1,2,3,4,5]
+const myheros = ["yog", "virat"]
+const myArr2 = new Array(1,2,3,4)
+//console.log(myArr[1]);
+
+// array methods
+myArr.push(6)
+myArr.push(7)
+//console.log(myArr);
+myArr.pop()
+//console.log(myArr);
+myArr.unshift(2)
+//console.log(myArr);
+myArr.shift()
+//console.log(myArr);
+//console.log(myArr.includes(9));
+//console.log(myArr.indexOf(3));
+const newArr = myArr.join()
+//console.log(myArr);
+console.log( newArr);
+// slice and splice
+//console.log("A " , myArr);
+const myn1 = myArr.slice(1,3)
+//console.log(myn1);
+//console.log("b",myArr);
+const myn2 = myArr.splice(1,3)
+//console.log("c ",myArr);
+//console.log(myn2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
