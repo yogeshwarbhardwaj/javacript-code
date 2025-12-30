26941c11e060577e5529b1de03a71d8e2f711204
yogeshwarbhardwaj <157011412+yogeshwarbhardwaj@users.noreply.github.com>
2025-12-30 14:53:20 +0000

    objects done

---
+++ 02_basics/object05.js
@@ -0,0 +1,33 @@
+const course = {
+    coursename: "js in hindi",
+    price:"1000",
+    courseinstructor: "yogeshwar"
+}
+//console.log(course.courseinstructor);
+const {courseinstructor:instructor} = course
+console.log(instructor);
+// destructure of objects 
+// this is not for js this for react where we have to remove promps like promps.company
+
+//const navbar = (company) => {
+
+//}
+//navbar(comapny ="bhardwaj")
+// api in jason format 
+//{
+    //"name " : "yogeshwar bhardwaj",
+    //"course name ": "js yt"
+   // "price" : "free"
+
+
+//}
+//apis are not always in object form sometimes it can be in array form in js
+[
+    {},
+    {},
+    {}
+]
+
+
+
+
