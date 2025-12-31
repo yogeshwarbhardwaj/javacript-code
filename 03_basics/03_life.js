36b523a2d5064d614ef605b911f8ef92b93abd1f
yogeshwarbhardwaj <157011412+yogeshwarbhardwaj@users.noreply.github.com>
2025-12-31 06:29:38 +0000

    iife done

---
+++ 03_basics/03_life.js
@@ -0,0 +1,11 @@
+// immediately invoked function expression(IIfe)
+(function chai(){
+    console.log(`DB CONNECTED`);
+    
+})();
+
+((name) => {
+    console.log(`db connected two ${name}`);
+    
+}) ('yogeshwar')
+
