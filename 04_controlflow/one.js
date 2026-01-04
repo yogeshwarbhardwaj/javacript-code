10cb7277bc7a387fec98af8deb3565c37426ec1b
yogeshwarbhardwaj <157011412+yogeshwarbhardwaj@users.noreply.github.com>
2026-01-04 03:28:37 +0000

    one control flow done

---
+++ 04_controlflow/one.js
@@ -0,0 +1,53 @@
+// if 
+const isuserloggedin = true 
+//if(2 == "2"){
+    //console.log("executed");
+//}
+// <,>, <= ,>=, ==, !=,===
+//const temperature = 41
+//if(temperature < 50){
+    //console.log("less than 50");    
+//}else{
+//console.log("temperature is greater than 50");
+//}
+//const score = 200
+//if(score > 100){
+  //  const power ="fly"
+    //console.log(`user power:${power}`); 
+//}
+//console.log(`user power:${power}`);
+
+const balance = 1000
+//if(balance > 500) console.log("test"),console.log("test2")
+//if(balance < 500){
+    //console.log("less than 500");
+    
+//}else if(balance < 750){
+  //  console.log("less than 750");
+    
+//}else if(balance < 900){
+  //  console.log("less than 900");
+    
+//}
+//else{
+  //  console.log("less than 1200");
+    
+//}
+const userloggedin = true 
+const debitcard = true
+const loggedinfromgoogle = false
+const loggedinfromemail = true
+if(userloggedin && debitcard ){
+    console.log("allow to buy course");  
+}
+if(loggedinfromgoogle || loggedinfromemail){
+    console.log("user logged in");
+    
+}
+
+
+
+
+ 
+
+ 
