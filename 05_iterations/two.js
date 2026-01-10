949a4db5dc542f3fd1ec9c8717d1cdab2c730d3c
yogeshwarbhardwaj <157011412+yogeshwarbhardwaj@users.noreply.github.com>
2026-01-10 03:32:32 +0000

    while and dowhile loop done

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+// while loop
+let index = 0
+//while (index < 20) {
+    //console.log(index);
+   // index++
+    
+    
+//}
+let myarray = ["msdhoni", "suresh raina","rohit sharma"]
+let arr = 0
+while (arr<myarray.length) {
+    //console.log(myarray[arr]);
+    arr++
+    
+    
+}
+
+//do while
+let score = 1
+do{
+    console.log(`score is ${score}`);
+    score++
+    
+}while(score <= 10)
