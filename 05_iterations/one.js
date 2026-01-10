a48f1cff1d5c97fca448898791581183ce4e0865
yogeshwarbhardwaj <157011412+yogeshwarbhardwaj@users.noreply.github.com>
2026-01-10 02:56:09 +0000

    basics of iteration done

---
+++ 05_iterations/one.js
@@ -0,0 +1,58 @@
+// loop
+for (let i = 0; i < 10; i++) {
+    //const element = i;
+    if(i == 5){
+        //console.log("5 is best number");
+        
+    }
+    //console.log(element);
+    
+    
+}
+//console.log(element);
+
+// nested loop
+for (let i = 0; i <= 10; i++) {
+    //console.log(`outer loop is :${i}`);
+    for (let j = 0; j < 10; j++) {
+        //console.log(`inner loop is ${j} and outer loop is ${i}`);
+        //console.log(i + '*' + j + ' =' + i*j);
+        
+        
+        
+    }
+}
+
+    let myarray = ["major mohit sharma" , "ravindra kaushik"]
+    for (let index = 0; index < myarray.length; index++) {
+        const element = myarray[index];
+        console.log(element);
+        
+        
+    }
+    // break and continue 
+   // for (let index = 1; index <= 20; index++) {
+       // if(index == 5){
+            //console.log(`detected 5`);
+            //break;
+            
+        //}
+        //console.log(`value of i is ${index}`);
+        
+    //}
+
+    for (let index = 1; index <= 20; index++) {
+        if(index == 5){
+            console.log(`detected 5`);
+            continue
+            
+            
+        }
+        console.log(`value of i is ${index}`);
+        
+    }
+    
+    
+    
+    
+
